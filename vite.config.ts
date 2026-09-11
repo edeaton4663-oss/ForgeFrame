8c13c1272823a1920e3be96ba54fa46ff701183c
edeaton4663-oss <edeaton4663@spanola.net>
2026-09-10 19:32:51 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    allowedHosts: true
+    allowedHosts: 'all'
   },
   preview: {
     host: '0.0.0.0',
-    allowedHosts: true
+    allowedHosts: 'all'
   }
 });
