ead89d41309539bebd2218e9218771a9b6db0f3f
edeaton4663-oss <edeaton4663@spanola.net>
2026-09-10 21:38:26 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // Sets the base URL path to match your GitHub repository name
-  base: '/ForgeFrame/',
   plugins: [react()],
+  base: '/', 
   server: {
     host: '0.0.0.0',
     allowedHosts: true
