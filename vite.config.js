00be5c5e613737f085ae638574517c59164868c8
edeaton4663-oss <edeaton4663@spanola.net>
2026-09-10 21:48:59 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: '/',
+  // Prevents Vite from crashing if your Supabase keys are not added yet
+  define: {
+    'process.env': {}
+  },
   server: {
     host: '0.0.0.0',
     allowedHosts: true
